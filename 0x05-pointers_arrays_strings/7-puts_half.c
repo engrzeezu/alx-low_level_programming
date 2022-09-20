392ea57faa9e327604d82a548b97727eb0ea3964
engrzeezu <abdulazeezilyasu@gmail.com>
2022-09-20 01:15:51 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * puts_half - function with one argument
+ * @str: char type
+ *
+ * Description: prints half of a string
+ * Return: na
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
