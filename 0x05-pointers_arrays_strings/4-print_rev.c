97a44a50ef9ad4ee1d3d10967a774247e2b29977
engrzeezu <abdulazeezilyasu@gmail.com>
2022-09-20 03:51:21 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,29 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_rev - function that prints a string, in
- * reverse followed by new line to stdout
- * @s: string to print
- * Return: void
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: 0
  */
 
 void print_rev(char *s)
 {
-	int i, j;
-	char c;
+	int i = 0;
 
-	i = 0;
-	while (*(s + i))
-	{
+	while (s[i])
 		i++;
-	}
-	c = *(s + i - 1);
-	j = 1;
-	while (c)
-	{
-		_putchar(c);
-		j++;
-		c = *(s + i - j);
-	}
+	while (i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
